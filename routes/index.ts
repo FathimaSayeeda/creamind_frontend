cc81df792461541b9612b8f42b891682ba1f257c
Fahim <faztp12@gmail.com>
2021-07-18 22:05:45 +0530

    fix: generate routes fixes

--- routes/index.ts
+++ routes/index.ts
@@ -1,4 +1,6 @@
-export async function getRoutes() {
+import axios from "axios";
+
+export async function getRoutes(...args: any[]) {
   const routes: String[] = ['/', '/books']
   const books = await getAllBooks()
 
@@ -6,6 +8,28 @@ export async function getRoutes() {
   return routes
 }
 
-async function getAllBooks() {
-  return ['Book A']
+async function getAllBooks(): Promise<string[]> {
+  const query = `
+  {
+    Books(first: 999999) {
+      edges {
+        node {
+          name
+        }
+      }
+    }
+  }
+  `
+  const json_gql = {
+    query,
+  }
+  const r = await axios.post(`${process.env.THECREAMIND_API}/api/method/graphql`, json_gql)
+  console.log(r.data)
+  return r.data.data.Books.edges.map((x: {node: {name: string }}) => x.node.name)
 }
+
+// async function test() {
+//   console.log(await getAllBooks())
+// }
+
+// test()
